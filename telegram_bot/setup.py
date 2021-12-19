fc2564504d6d0f12a07b11587956e2de1100a02c
miloft <miloft@yandex.ru>
2021-12-20 00:49:56 +0300

    обновлен requirements.txt, добавлен файл с ключами

---
+++ telegram_bot/setup.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+TOKEN = ''  # bot token
+# Optionally use an anonymizing proxy (SOCKS/HTTPS) if you encounter Telegram access issues in your region
+PROXY = ''
+
+API_KEY = '',
+API_SECRET = ''
