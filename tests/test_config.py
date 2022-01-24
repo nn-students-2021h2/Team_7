cce7da4d989ba87a24d33c3ffda725a633b1c7be
John <is.boldyrev@yandex.ru>
2022-01-24 18:31:01 +0300

    Изменены тесты

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,6 +1,6 @@
 import unittest
 
-from telegram_bot.config import ConfigSingleton, TOKEN, FACE_PP_API_KEY, FACE_PP_API_SECRET, RAPID_API_KEY
+from telegram_bot.config import ConfigSingleton, TOKEN, FACE_PP_API_KEY, FACE_PP_API_SECRET, RAPID_API_KEY, json_path
 
 
 class MyTestCase(unittest.TestCase):
@@ -12,16 +12,13 @@ class MyTestCase(unittest.TestCase):
         self.assertTrue(FACE_PP_API_KEY)
         self.assertTrue(FACE_PP_API_SECRET)
         self.assertTrue(RAPID_API_KEY)
+        self.assertTrue(json_path)
 
     def test_config_class(self):
         self.assertTrue(hasattr(self.config, 'total_max_requests'))
         self.assertTrue(hasattr(self.config, 'current_requests'))
         self.assertTrue(hasattr(self.config, 'rapid_url'))
         self.assertTrue(hasattr(self.config, 'face_pp_url'))
-
-        self.assertEqual(self.config.total_max_requests, 4980)
-        self.assertEqual(self.config.rapid_url, 'https://face-detection6.p.rapidapi.com/img/face-age-gender')
-        self.assertEqual(self.config.face_pp_url, 'https://api-us.faceplusplus.com/facepp/v3/detect')
 
 
 if __name__ == '__main__':
