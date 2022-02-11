4682d9d436d69d5736eeb185a143d846e4a254f6
John <is.boldyrev@yandex.ru>
2022-02-11 10:05:27 +0300

    Работа с сессией через контекстный менеджер

---
+++ db/models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, BigInteger
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+# таблица с юзерами
+class User(Base):
+    __tablename__ = 'Users'
+
+    user_id = Column(BigInteger, primary_key=True)
+    user_name = Column(String(250))
+    full_name = Column(String(250))
+    requests_count = Column(Integer, default=0)
